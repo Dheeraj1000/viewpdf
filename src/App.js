84411cc4e0e6c0df37d578311ddd48e0dad3905f
Dheeraj1000 <93862893+Dheeraj1000@users.noreply.github.com>
2024-04-01 01:10:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import './App.css';
+
 
 function App() {
+
+  const pdfPath = '/pages.pdf';
+
+
+  const handeView = () => {
+    window.open(pdfPath,'_blank')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <iframe src='https://drive.google.com/file/d/1IbMkx7b0byMv2Wyky_cJ1yOS8tCoxhvN/view?usp=sharing' title='View Document' width="100%" height="600px"></iframe>
-      </header>
+    <div style={{display : 'flex', flexDirection: 'column', width: '100%', alignItems:'center', padding:'30px'}}>
+     <h3  style={{textAlign:'center'}}> Confidentiality Agreement </h3>
+     <h3  style={{textAlign:'center'}}>is Made By And between </h3>
+     <h1  style={{textAlign:'center'}}> Mrs. Manasa BV (1st Party)</h1>
+     <h3  style={{textAlign:'center'}}>AND</h3>
+     <h1  style={{textAlign:'center'}}> Mr.Sanapala Yugandar (2nd Party)</h1>
+     <h3 style={{textAlign:'center'}}>Documents and Information Related to, and Limited to File No.288 B , Segment 3D, Quotation AAB.</h3>
+     <br/>
+     <p  style={{textAlign:'center'}}>CC No. 188/02.05.029/2023-24</p>
+         <button onClick={handeView} type="button" class="btn btn-primary" style={{padding: '20px'}}>View Document</button>
     </div>
   );
 }
